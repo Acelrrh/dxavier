3e95fb68a75590b0fbb0aec6cf8d40c6b811e44c
artanodrop <114789247+artanodrop@users.noreply.github.com>
2022-11-09 09:49:47 +0700

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -71,9 +71,9 @@ sleep 1
 cp /root/backup/ss.conf /etc/shadowsocks-libev/ss.conf &> /dev/null
 echo -e "$COLOR1│${NC}  [ ${green}INFO${NC} ] • Restoring admin data..."
 sleep 1
-cp -r /root/backup/yokkovpn-pro /var/lib/ &> /dev/null
+cp -r /root/backup/artanodrop-pro /var/lib/ &> /dev/null
 cp -r /root/backup/.acme.sh /root/ &> /dev/null
-cp -r /root/backup/yokkovpn /etc/ &> /dev/null
+cp -r /root/backup/artavpn /etc/ &> /dev/null
 cp -r /root/backup/xray /etc/ &> /dev/null
 cp -r /root/backup/public_html /home/vps/ &> /dev/null
 cp -r /root/backup/crontab /etc/ &> /dev/null
